981a68be7e6c5abd89e092a61e62dc9ae25f7da1
Belle <103557809+yxmauw@users.noreply.github.com>
2022-10-11 00:08:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ new_img = st.file_uploader('PLease upload your retinal image')
 def predict_upload():
   result = predict(new_img) # result is a probabilities array
   classes = ['cataract', 'diabetic_retinopathy', 'glaucoma', 'normal']
+  st.write(np.max(result, axis=0))
   pred_prob = np.format_float_positional(np.max(result, axis=0), precision=4) # max probability
   pred_class = classes[np.argmax(result, axis=-1)] # string
   st.success(f'There is a {pred_prob} that this retinal image shows {pred_class}')
