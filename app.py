231d1e67e15317fb4fc4ebb169414109ee7d42cf
Belle <103557809+yxmauw@users.noreply.github.com>
2022-10-11 00:39:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -18,14 +18,10 @@ new_img = st.file_uploader('PLease upload your retinal image')
 
 def predict_upload():
   result = predict(new_img) # result is a probabilities array 
-  st.write(result[0]) 
-  st.write(result.shape)
-  st.write(type(result))
-  st.write(np.argmax(result, axis=-1)[0])
   classes = ['cataract', 'diabetic_retinopathy', 'glaucoma', 'normal']
-  pred_prob = np.format_float_positional(np.max(result, axis=-1), precision=4) # max probability
+  pred_prob = np.format_float_positional(np.max(result, axis=-1), precision=4) * 100 # max probability
   pred_class = classes[(np.argmax(result, axis=-1)[0])] # string
-  st.success(f'There is a {pred_prob} that this retinal image shows {pred_class}')
+  st.success(f'There is a {pred_prob} % probability that this retinal image shows {pred_class}')
   
 # instantiate submit button
 if st.button('Submit'):
