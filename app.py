3dfb38f7a52c3b08278e68943e475f5a61889b94
Belle <103557809+yxmauw@users.noreply.github.com>
2022-10-11 00:42:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def predict_upload():
   max_result = (np.max(result, axis=-1)) * 100 # max probability
   pred_prob = np.format_float_positional(max_result, precision=2) # format probability
   pred_class = classes[(np.argmax(result, axis=-1)[0])] # string
-  st.success(f'There is a {pred_prob} % probability that this retinal image shows {pred_class}')
+  st.success(f'There is a **{pred_prob}** % probability that this retinal image shows **{pred_class}**')
   
 # instantiate submit button
 if st.button('Submit'):
