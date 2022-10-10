ca7b200dd2ddc010a7286f52571533a2443b99de
Belle <103557809+yxmauw@users.noreply.github.com>
2022-10-10 22:24:33 +0800

    Update model_methods.py

--- model_methods.py
+++ model_methods.py
@@ -1,25 +1,28 @@
 import numpy as np
 import tensorflow as tf
 import streamlit as st
-
-@st.cache
-def load_model():  # Load the TFLite model and allocate tensors.
-   with open('ENet_model.tflite','rb') as f:
-        model = tf.lite.Interpreter(model_content=f)
-        model.allocate_tensors()
-   return model
+import cv2
 
 def predict(image):
-  # read image
-  # Convert the compressed string to a 3D uint8 tensor
-  img = tf.io.decode_image(image, channels=3)
-  # Resize the image to the desired size
-  img = tf.image.resize(img, [160, 160])
-  
-  # preprocess new image
-  img = tf.keras.applications.efficientnet_v2.preprocess_input(img)
-  
-  # load model if not previously loaded
-  model = load_model()
-  pred = model.predict(img)
-  return pred
+   with open('ENet_model.tflite','rb') as f:
+        interpreter = tf.lite.Interpreter(model_content=f)
+        interpreter.allocate_tensors()
+        #get input and output tensors
+        input_details = interpreter.get_input_details()
+        output_details = interpreter.get_output_details()
+        # read image
+        # Convert the compressed string to a 3D uint8 tensor
+        img = tf.io.decode_image(image, channels=3)
+        # Resize the image to the desired size
+        img = tf.image.resize(img, [160, 160])
+        #Preprocess the image to required size and cast
+        input_shape = input_details[0]['shape']
+        input_tensor= np.array(np.expand_dims(img,0), dtype=np.float32)
+        input_tensor= tf.keras.applications.efficientnet_v2.preprocess_input(input_tensor)
+        #set the tensor to point to the input data to be inferred
+        input_index = interpreter.get_input_details()[0]["index"]
+        interpreter.set_tensor(input_index, input_tensor)
+        #Run the inference
+        interpreter.invoke()
+        output_details = interpreter.get_output_details()[0]  
+   return output_details
