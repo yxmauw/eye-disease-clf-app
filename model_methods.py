4d61f63c7e6d5f82deb45a1045ea212e68f45457
Belle <103557809+yxmauw@users.noreply.github.com>
2022-10-11 20:14:57 +0800

    Update model_methods.py

--- model_methods.py
+++ model_methods.py
@@ -89,7 +89,7 @@ import cv2
 
 def gradCAM(orig, intensity=0.5, res=250): # function
   #img = Image.open(io.BytesIO(orig.read())
-  img = Image.open(orig.read())
+  img = Image.open(io.BytesIO(orig))
   img = img.convert('RGB')
   # Resize the image to the desired size
   img = img.resize((160,160))
