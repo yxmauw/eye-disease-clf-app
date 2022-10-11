31650c5717ebf68a52849467622a822c3611fb45
Belle <103557809+yxmauw@users.noreply.github.com>
2022-10-11 22:20:27 +0800

    Update model_methods.py

--- model_methods.py
+++ model_methods.py
@@ -123,4 +123,6 @@ def gradCAM(orig, intensity=0.5, res=250): # function
   img = heatmap * intensity + img
   
   img1 = cv2.resize(img, (res, res)) # visualise heatmap overlay
-  st.image(img1)
+  cv2.imwrite('temporary.jpg', img1)
+  st.image('temporary.jpg')
+  #st.image(img1)
