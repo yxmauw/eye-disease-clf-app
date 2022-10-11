db072b8bc4937c0d2a915132b4453342184e57be
Belle <103557809+yxmauw@users.noreply.github.com>
2022-10-11 16:32:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -50,4 +50,6 @@ if st.button('Classify'):
             max_score = tf.math.reduce_max(result_img[0,max_idx[0]]) # tensor max probability
             #max_score = result_img[0,max_idx[0]]
         st.write(tape.gradient(max_score, input_im))
+        st.write(max_score)
+        st.write(input_im)
         #plot_maps(normalize_image(grads[0]), normalize_image(input_im[0]))
