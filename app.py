ebf586d1402993c120fc440bdeb0ecb071b33169
Belle <103557809+yxmauw@users.noreply.github.com>
2022-10-15 21:43:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -57,5 +57,5 @@ if st.button('Classify'):
              \n\n Thank you 🙏
              ''')
 
-st.write('##')            
+st.write('##')   # create space         
 st.write('DISCLAIMER: THIS WEBSITE DOES NOT REPLACE MEDICAL ADVICE \n\n The information, including but not limited to, text, graphics, images and other material contained on this website are for informational purposes only. No material on this site is intended to be a substitute for professional medical advice, diagnosis or treatment. Always seek the advice of a physician or other qualified health care provider with any questions you may have regarding a medical condition or treatment and before undertaking a new health care regimen, and never disregard professional medical advice or delay in seeking it because of something you have read on this website.')
